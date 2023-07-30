994a6d00f341766249d65e2a3daf8d99dabe4ea7
Weng Shiwei <arbipher@gmail.com>
2023-07-30 17:52:52 -0400

    add styling test

--- prettyprint/bin/sp2.ml
+++ prettyprint/bin/sp2.ml
@@ -1,1 +1,132 @@
-let () = ()
+(* module type AT = sig
+     type t
+
+     val v : t
+   end
+
+   module A : AT = struct
+     type t = int
+
+     let v : t = 3
+   end
+
+   module type BT = sig
+     type t = int
+
+     val v : t
+   end
+
+   module B : BT = struct
+     (* include A *)
+     type t = int
+
+     let v = 4
+   end
+
+   let foo (a : A.t) : B.t = Obj.magic a
+   let () = print_int (foo A.v) *)
+
+(* --- *)
+
+module type A = sig
+  type t
+end
+
+module type B = sig
+  type u
+end
+
+module type C = sig
+  module X : A
+  module Y : B
+
+  type t
+end
+
+module F (Z : C with type X.t = int and type Y.u = bool and type t = int) =
+struct
+  module Z = Z
+end
+
+module Imp_A : A with type t = int = struct
+  type t = int
+end
+
+module Imp_B : B with type u = bool = struct
+  type u = bool
+end
+
+module Imp_C : C with type X.t = int and type Y.u = bool and type t = int =
+struct
+  module X = Imp_A
+  module Y = Imp_B
+
+  type t = int
+end
+
+module Imp_F = F (Imp_C)
+
+(* --- *)
+module type A2 = sig
+  type t
+end
+
+module type B2 = sig
+  type u
+end
+
+(*
+module type C2 = sig
+  module X2 : A2
+  module Y2 : B2
+end
+with 
+ type X2.t = Y2.u *)
+
+module type A3 = sig
+  type t
+end
+
+module type B3 = sig
+  type u
+end
+
+(* module type C3 = sig
+     module X3 : A3
+     module Y3 : B3
+
+     type v
+   end
+   with type v = X3.t
+    and type v = Y3.u *)
+
+module type J = sig
+  type p
+  type q
+
+  val f : p -> q -> p * q
+end
+
+(* module G (H : J with type p = q) = struct
+     module H = H
+   end *)
+
+module type A4 = sig
+  type t
+end
+
+module type B4 = sig
+  type u
+end
+
+module type C4 = sig
+  module X4 : A4
+  module Y4 : B4
+end
+
+module F4 (X4 : A4) (Y4 : B4 with type u = X4.t) = struct
+  module Z4 : C4 = struct
+    module X4 = X4
+    module Y4 = Y4
+  end
+end
--- prettyprint/styling.ml
+++ prettyprint/styling.ml
@@ -1,3 +1,15 @@
+(* observe
+   for `let x = e1 in e2`,
+   - `let x = ` is always at the first line
+   - `e2` is always at the last line
+   - `e1 in` will either be first in the first line or
+   - `e1` will be in the indented new line and `in` will be in the non-indented newline. so the structure will be
+*)
+
+let%expect_test _ =
+  Fmt.int Fmt.stdout @@ Format.pp_get_margin Format.std_formatter () ;
+  [%expect {| 78 |}]
+
 module Dummy = struct
   let _x = 1 in
   let _y =
@@ -32,18 +44,128 @@ let _ =
   y1
 
 let%expect_test _ =
-  Fmt.pr "@[<v>@[<v 2>let y1 =@ @[<v>@[<v 2>let y2 = 2@]@ in@ y2@]@]@ in@ y1@]" ;
-  (*      o1----------------------------------------------------------------o1 *)
+  Fmt.pr "@[<v>@[<v 2>let y1 =@ @[<v 2>let y2 = 2@] in@ y2@]@ in@ y1@]" ;
   [%expect {|
-    let y1 =
-      let y2 = 2
-      in
-      y2
+  let y1 =
+    let y2 = 2 in
+    y2
+  in
+  y1 |}]
+
+let _ =
+  let z1 =
+    let z2 =
+      let z3 = 2 in
+      z3
     in
-    y1 |}]
+    z2
+  in
+  z1
 
 let%expect_test _ =
-  Fmt.pr "@.%s\n world!!!" "Hello," ;
+  Fmt.pr
+    "@[<v>@[<v 2>let z1 =@ @[<v 2>let z2 =@ let z3 = 2 in z3@]@ in@ z2@]@ in@ \
+     z1@]" ;
+  [%expect
+    {|
+  let z1 =
+    let z2 =
+      let z3 = 2 in z3
+    in
+    z2
+  in
+  z1 |}]
+
+let _ =
+  let y1 =
+    let y2 =
+      "222222222222222222222222222222222222222222222222222222222222222"
+    in
+    y2
+  in
+  y1
+
+let%expect_test _ =
+  Fmt.pr
+    "@[<v>@[<v 2>let y1 =@ @[<v 2>let y2 = \
+     22222222222222222222222222222222222222222222222222222222222222222222222222222222222@] \
+     in@ y2@]@ in@ y1@]" ;
+  [%expect
+    {|
+  let y1 =
+    let y2 = 22222222222222222222222222222222222222222222222222222222222222222222222222222222222 in
+    y2
+  in
+  y1 |}]
+
+let _ =
+  let z1 =
+    let z2 =
+      let z3 =
+        "2222222222222222222222222222222222222222222222222222222222222"
+      in
+      z3
+    in
+    z2
+  in
+  z1
+
+let%expect_test _ =
+  Fmt.pr "@[<v>pre body@ in@ post" ;
   [%expect {|
-    Hello,
-     world!!! |}]
+  pre body
+  in
+  post |}] ;
+  Fmt.pr "@[<v>@[<v 2>pre@ body_new_line@]@ in@ post@]" ;
+  [%expect {|
+  pre
+    body_new_line
+  in
+  post |}] ;
+  Fmt.pr
+    "@[<v>@[<v 2>pre@ @[<v>@[<v 2>pre@ body_new_line@]@ in@ post@]@]@ in@ \
+     post@]" ;
+  [%expect {|
+  pre
+    pre
+      body_new_line
+    in
+    post
+  in
+  post |}] ;
+  Fmt.pr
+    "@[<v>@[<v 2>pre@ @[<v>@[<v 2>pre@ @[<v>@[<v 2>pre@ body_new_line@]@ in@ \
+     post@]@]@ in@ post@]@]@ in@ post@]" ;
+  [%expect
+    {|
+pre
+  pre
+    pre
+      body_new_line
+    in
+    post
+  in
+  post
+in
+post |}] ;
+
+  Fmt.pr "@[<v>@[<v 2>pre body_same_line@]@ in@ post@]" ;
+  [%expect {|
+pre body_same_line
+in
+post |}] ;
+  Fmt.pr
+    "@[<v>@[<v 2>pre@ @[<v>@[<v 2>pre body_same_line@]@ in@ post@]@]@ in@ \
+     post@]" ;
+  [%expect {|
+  pre
+    pre body_same_line
+    in
+    post
+  in
+  post |}] ;
+  ()
+
+(* observe:
+   if you are a nested block element, print a leading `@ ` (because you are surrounded in a outer <v> block)
+*)
